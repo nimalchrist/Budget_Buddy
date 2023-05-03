c5123b5c6969e0361901fa29181ad5fb7c29bb96
nimalchrist <chiristonimal@gmail.com>
2023-05-03 22:00:23 +0530

    aws db config

--- back_end/services/db.js
+++ back_end/services/db.js
@@ -2,10 +2,10 @@ const mysql = require("mysql2");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: "budget-buddy-db.caxjmojddnbu.us-east-1.rds.amazonaws.com",
+  user: "admin",
+  password: "Ninunimal",
+  database: "budget-buddy-db",
 });
 
 connection.connect(function (err) {
